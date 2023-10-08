102b94cf879f85427c4c719c6acfbefb9cd0114d
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2023-10-08 20:14:35 +0200

    nav-bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomescreenComponent } from './homescreen/homescreen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomescreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
