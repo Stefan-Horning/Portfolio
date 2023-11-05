c14c9d372a0ddc8168c53983e54fc818aec92524
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2023-11-05 16:26:28 +0100

    fotter

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  screenWidth:number;
+  constructor() {
+    this.screenWidth = window.innerWidth;
+    this.getScreenSize();
+  }
 
+  @HostListener('window:resize', [])
+  getScreenSize() {
+      this.screenWidth = window.innerWidth;
+  }
 }
